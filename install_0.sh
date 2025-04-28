8ea207f7673b63ab81fee3b28e78ec92bcb93cd2
Sardinha CN <50783766+SardinhaCN@users.noreply.github.com>
2025-04-28 11:41:48 -0300

    Update install_0.sh

--- install_0.sh
+++ install_0.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Atualizar dependencias
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 #instalação do nodejs e npm
-sudo apt install -y nodejs npm
+apt install -y nodejs npm
 node -v
 npm -v
 
 #Preparando Anbiente Develope!
-sudo npm i -g -y pm2 express express-generator ejs nodemon
+npm i -g -y pm2 express express-generator ejs nodemon
 #Montando o Projeto
 mkdir projeto
 ls -d
 cd ./projeto
-sudo express --ejs --view=ejs --css=stylus --git
+express --ejs --view=ejs --css=stylus --git

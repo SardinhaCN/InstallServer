8bf4ee0e374003d8497eb6a854b543dab3e845cf
Sardinha CN <50783766+SardinhaCN@users.noreply.github.com>
2025-05-05 21:41:57 -0300

    Update config_server.sh

--- config_server.sh
+++ config_server.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#Atualizar dependencias
+echo "Atualizar dependencias"
 sudo apt update
 sudo apt upgrade -y
 
 clear
-
+echo "Clonando repositorio 'InstallServer'"
 cd ..
 cd ..
 git clone https://github.com/SardinhaCN/InstallServer.git
 
 clear
 
-#instalação do nodejs e npm
+echo "instalação do nodejs e npm"
 sudo apt install -y nodejs npm
 node -v
 npm -v
 
 clear
 
-#Preparando Anbiente Develope!
+echo "Preparando Anbiente Develope!"
 sudo npm i -g -y pm2 express express-generator ejs nodemon
-#Montando o Projeto
+echo "Montando o Projeto"
 mkdir projeto
 ls -d
 cd ./projeto
@@ -28,5 +28,10 @@ sudo express --ejs --view=ejs --css=stylus --git
 cd ..
 
 clear
-
+echo "Instalando CasaOS!"
 curl -fsSL https://get.casaos.io | sudo bash
+
+clear
+echo "Configurando IP Static"
+mv -i /InstallServer/50-cloud-init.yaml /etc/netplan/
+netplan apply

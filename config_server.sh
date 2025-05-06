acef7ce5be91040ad6f93184766c8c43460d01d6
Sardinha CN <50783766+SardinhaCN@users.noreply.github.com>
2025-05-05 21:56:12 -0300

    Update config_server.sh

--- config_server.sh
+++ config_server.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
+clear
 echo "Atualizar dependencias"
 sudo apt update
 sudo apt upgrade -y
 
-clear
-echo "Clonando repositorio 'InstallServer'"
-cd ..
-cd ..
-git clone https://github.com/SardinhaCN/InstallServer.git
+# Função para exibir o menu
+mostrar_menu() {
+    echo "===== MENU DE APP ====="
+    echo "1) Preparando Anbiente Develope NodeJS!"
+    echo "2) Instalar o CasaOS!"
+    echo "3) Teste de velocidade da internet (speedtest-cli)"
+    echo "4) S-Tui - Monitor de CPU gráfico via terminal"
+    echo "0) Sair"
+    echo "==========================="
+}
 
-clear
+# Mostra o menu uma vez
+mostrar_menu
+read -p "Escolha uma opção: " opcao
 
-echo "instalação do nodejs e npm"
-sudo apt install -y nodejs npm
-node -v
-npm -v
-
-clear
-
-echo "Preparando Anbiente Develope!"
-sudo npm i -g -y pm2 express express-generator ejs nodemon
-echo "Montando o Projeto"
-mkdir projeto
-ls -d
-cd ./projeto
-sudo express --ejs --view=ejs --css=stylus --git
-cd ..
-
-clear
-echo "Instalando CasaOS!"
-curl -fsSL https://get.casaos.io | sudo bash
-
-clear
-echo "Configurando IP Static"
-mv -i /InstallServer/50-cloud-init.yaml /etc/netplan/
-netplan apply
+# Executa a opção selecionada
+case $opcao in
+    2)
+        clear
+        echo "instalação do nodejs e npm"
+        cd ..
+        cd ..
+        sudo apt install -y nodejs npm
+        node -v
+        npm -v
+        echo "Preparando Anbiente Develope!"
+        sudo npm i -g -y pm2 express express-generator ejs nodemon
+        echo "Montando o Projeto"
+        mkdir projeto
+        ls -d
+        cd ./projeto
+        sudo express --ejs --view=ejs --css=stylus --git
+        cd ..
+      ;;
+    3)
+        clear
+        echo "Instalando CasaOS!"
+        curl -fsSL https://get.casaos.io | sudo bash
+      ;;
+    3)
+        clear
+        echo "Clonando repositorio 'InstallServer'"
+        cd ..
+        cd ..
+        git clone https://github.com/SardinhaCN/InstallServer.git
+        clear
+        echo "Configurando IP Static"
+        mv -i /InstallServer/50-cloud-init.yaml /etc/netplan/
+        netplan apply
+      ;;
+    
+    *)
+        echo "Opção inválida! Tente novamente."
+        ;;
+esac

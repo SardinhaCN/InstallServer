fc3be25c6ac537ba78fa29c185d51c4c55793234
Sardinha CN <50783766+SardinhaCN@users.noreply.github.com>
2025-05-13 17:33:50 -0300

    Update config_server.sh

--- config_server.sh
+++ config_server.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
 clear
-echo "Atualizar dependencias"
+echo "Atualizando dependências..."
 sudo apt update
 sudo apt upgrade -y
 clear
+
 # Função para exibir o menu
 mostrar_menu() {
     echo "===== MENU DE APP ====="
-    echo "1) Preparando Anbiente Develope NodeJS!"
-    echo "2) Instalar o CasaOS!"
-    echo "3) Configurando IP Static!"
+    echo "1) Preparar Ambiente NodeJS"
+    echo "2) Instalar CasaOS"
+    echo "3) Configurar IP Estático"
     echo "0) Sair"
-    echo "==========================="
+    echo "========================="
 }
 
-# Mostra o menu uma vez
+# Mostrar menu
 mostrar_menu
 read -p "Escolha uma opção: " opcao
 
-# Executa a opção selecionada
+# Executar opção
 case $opcao in
     1)
         clear
-        echo "instalação do nodejs e npm"
-        cd ..
-        cd ..
+        echo "Instalando NodeJS e NPM..."
         sudo apt install -y nodejs npm
+
+        echo "Versões instaladas:"
         node -v
         npm -v
-        echo "Preparando Anbiente Develope!"
-        sudo npm i -g -y pm2 express express-generator ejs nodemon
-        echo "Montando o Projeto"
-        mkdir projeto
-        ls -d
-        cd ./projeto
-        sudo express --ejs --view=ejs --css=stylus --git
-        cd ..
-      ;;
+
+        echo "Instalando pacotes globais..."
+        sudo npm install -g pm2 express express-generator ejs nodemon
+
+        echo "Criando projeto..."
+        mkdir -p ~/projeto
+        cd ~/projeto || exit
+        express --view=ejs --css=stylus --git
+        echo "Projeto criado em ~/projeto"
+        ;;
     2)
         clear
-        echo "Instalando CasaOS!"
+        echo "Instalando CasaOS..."
         curl -fsSL https://get.casaos.io | sudo bash
-      ;;
+        ;;
     3)
         clear
-        echo "Clonando repositorio 'InstallServer'"
-        cd ..
-        cd ..
+        echo "Clonando repositório InstallServer..."
+        cd ~ || exit
         git clone https://github.com/SardinhaCN/InstallServer.git
-        clear
-        echo "Configurando IP Static"
-        mv -i /InstallServer/50-cloud-init.yaml /etc/netplan/
-        netplan apply
-      ;;
-    
+
+        echo "Configurando IP Estático..."
+        sudo mv -i ~/InstallServer/50-cloud-init.yaml /etc/netplan/
+        sudo netplan apply
+        ;;
+    0)
+        echo "Saindo..."
+        exit 0
+        ;;
     *)
         echo "Opção inválida! Tente novamente."
         ;;
